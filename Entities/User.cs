7e4b6b5cab04d4ee48c3ee58c4d55b05abbc8acd
Melanie Spence <melzs0627@gmail.com>
2023-05-22 16:28:55 -0600

    Update User for uniformity

--- Entities/User.cs
+++ Entities/User.cs
@@ -6,8 +6,8 @@ namespace BudgetBucketsAPI.Entities
     public class User
     {
         public int Id { get; set; }
-        public string EmailAddress { get; set; }
-        public string Password { get; set; }
+        public string EmailAddress { get; set; } = String.Empty;
+        public string Password { get; set; } = String.Empty;
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
         public Role Role { get; set; }
