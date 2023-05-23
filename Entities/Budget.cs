2105e3fd88066a07bc79d1a414a44a5b39af9241
Melanie Spence <melzs0627@gmail.com>
2023-05-22 18:54:09 -0600

    Update Budget Category Profile

--- Entities/Budget.cs
+++ Entities/Budget.cs
@@ -1,11 +1,14 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace BudgetBucketsAPI.Entities
 {
 	public class Budget
 	{
-		public Budget()
-		{
-		}
+		public int Id {get; set;}
+		[ForeignKey("User")]
+		public int UserId {get; set;}
+		public DateTime Timeframe {get; set;}
 	}
 }
 
--- Entities/Profile.cs
+++ Entities/Profile.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace BudgetBucketsAPI.Entities
 {
 	public class Profile
 	{
 		public int Id { get; set; }
+		[ForeignKey("User")]
 		public int UserId { get; set; }
 		public string FirstName { get; set; } = String.Empty;
 		public string LastName { get; set; } = String.Empty;
